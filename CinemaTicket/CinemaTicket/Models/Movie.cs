1b63974d16a61494661c1e9a2eaa6dabcc1d76cd
tuntqce181083 <tuntqce181083@fpt.edu.vn>
2025-02-13 15:42:35 +0700

    Admin CRUD Movie CNT25-CNT29

--- CinemaTicket/CinemaTicket/Models/Movie.cs
+++ CinemaTicket/CinemaTicket/Models/Movie.cs
@@ -1,8 +1,23 @@
-﻿namespace CinemaTicketApp.Models
+﻿using System.ComponentModel;
+
+/// <summary>
+/// Ràng buộc Language tránh lỗi với sql
+/// Đặt ngoài namespace giúp sử dụng mà không cần khai báo using từ namespace
+/// </summary>
+public enum LanguageOption
 {
+    Subtitled,
+    Dubbed
+}
+namespace CinemaTicketApp.Models
+{
+
     public class Movie
     {
+        public LanguageOption enumLanguage;// cái này để chuyển đổi giá trị của language khi lưu hoặc thay đổi
+
         public int MovieID { get; set; }
+        //[DisplayName("Movie name")]
         public string Title { get; set; }
         public int Duration { get; set; }
         public string Language { get; set; }
